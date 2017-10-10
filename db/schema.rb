82f9b94431fffeded9feff3b8757a287e07fd8ac
Paul Lee <paul.lee@cantab.net>
2017-10-10 11:50:56 +0900

    Add content to Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010022851) do
+ActiveRecord::Schema.define(version: 20171010024945) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content"
   end
 
 end
